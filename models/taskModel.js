51ab3f48494d00cb21471318f5c30b7cd9a3145d
10alejospain <10alejo.est@gmail.com>
2021-10-19 13:22:36 +0200

    cambios modelos

--- models/taskModel.js
+++ models/taskModel.js
@@ -3,7 +3,14 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const taskSchema = new Schema({
+  userIdReference: { type: String },
   taskName: { type: String },
+  taskDescription: { type: String },
+  taskType: { type: String },
+  creationDate: { type: Date },
+  expireDate: { type: Date },
+  importanceLevel: { type: Number },
+  sharedIds: { type: Array },
 });
 
 module.exports = mongoose.model('Task', taskSchema);
---
+++ models/userModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: [true, 'Email required'],
+  },
+  name: {
+    type: String,
+    required: [true, 'Name required'],
+  },
+  surnames: {
+    type: String,
+    required: [true, 'Surname required'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password required'],
+  },
+  salt: {
+    type: String,
+    immutable: true,
+    required: [true, 'Can not continue without the salt'],
+  },
+  organization: {
+    type: String,
+  },
+  gender: {
+    type: String,
+    required: [true, 'Gender required'],
+    enum: ['male', 'female', 'other'],
+  },
+});
+module.exports = mongoose.model('User', userSchema);
