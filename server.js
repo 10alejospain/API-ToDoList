266e6858c5cdacfe64ab8efd13a96360bc267ad8
10alejospain <10alejo.est@gmail.com>
2022-10-25 17:17:42 +0200

    docker compose deployment not working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000; // Puerto asignado
+const PORT = 3000; // Puerto asignado
 const db = process.env.MONGODB || 'mongodb://localhost:27017/ToDo-API';
 
 mongoose.connect(db, { useNewUrlParser: true }, (err) => {
